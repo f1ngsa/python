3c13d182d4cae2be0da64b540ed7a1a629d8e56a
fedor buyanov <fdbuyanov@gmail.com>
2021-11-03 17:03:42 -0400

    story

---
+++ story.py
@@ -0,0 +1,2 @@
+print("You wake up on a spaceship, you dont remember anything. \nYour actions?\nOption 1 you contact Earth\nOr Option 2 you look around the space ship")
+enter = input("Which option are you choosing? option 1 or option 2?: ")
