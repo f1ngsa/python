81d1cae00e6243dfc475609884bcac80312aa070
fedor buyanov <fdbuyanov@gmail.com>
2021-11-10 16:52:31 -0500

    praktikum

--- practikum.py
+++ practikum.py
@@ -18,11 +18,39 @@
 # else:
 #     print("No")
 
-number = int(input("Enter a distance in meters: "))
-cmorkm = input("Do you want to convert it into centimeters or kilometers: ")
-if cmorkm == "Kilometers" or cmorkm == "kilometers" or cmorkm == "km" or cmorkm == "Km" or cmorkm == "KM":
-    print(number / 1000, "kilometers")
-elif cmorkm == "Centimeters" or cmorkm == "centimeters" or cmorkm == "cm" or cmorkm == "Cm" or cmorkm == "CM":
-    print(number * 100, "centimeters")
+# number = int(input("Enter a distance in meters: "))
+# cmorkm = input("Do you want to convert it into centimeters or kilometers: ")
+# if cmorkm == "Kilometers" or cmorkm == "kilometers" or cmorkm == "km" or cmorkm == "Km" or cmorkm == "KM":
+#     print(number / 1000, "kilometers")
+# elif cmorkm == "Centimeters" or cmorkm == "centimeters" or cmorkm == "cm" or cmorkm == "Cm" or cmorkm == "CM":
+#     print(number * 100, "centimeters")
+# else:
+#     print(number)
+
+# number = float(input("Enter a integer: "))
+# if number > 0:
+#     print("+")
+# elif number < 0:
+#     print("-")
+# else:
+#     print("0")
+
+# number = int(input("Enter a number: "))
+# if number % 2 == 0:
+#     print("Divisible by 2")
+# else:
+#     print("Not divisible by 2")
+
+firstnumber = float(input("Enter a number: "))
+secondnumber = float(input("Enter a number: "))
+problem = input("Do you want to subtract, divide, multiply, or add?: ")
+if problem == "-":
+    print(firstnumber - secondnumber)
+elif problem == "/" and secondnumber != 0:
+    print(firstnumber / secondnumber)
+elif problem == "*" or problem == "x":
+    print(firstnumber * secondnumber)
+elif problem == "+":
+    print(firstnumber + secondnumber)
 else:
-    print(number)
+    print("error")
