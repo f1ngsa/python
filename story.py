51115f9cade296181a0955755f1210fb5233e2ad
fedor buyanov <fdbuyanov@gmail.com>
2021-11-10 17:07:46 -0500

    story

--- story.py
+++ story.py
@@ -1,2 +1,6 @@
 print("You wake up on a spaceship, you dont remember anything. \nYour actions?\nOption 1 you contact Earth\nOr Option 2 you look around the space ship")
 enter = input("Which option are you choosing? option 1 or option 2?: ")
+if enter == "option 1" or enter == "Option 1" or enter == "1":
+    print("Earth isnt answering.")
+    print("Enter 1 if you want to send a SOS signal.\nEnter 2 if you want to press the big red button on the remote.")
+    hm = ("Which option are you choosing?: ")
