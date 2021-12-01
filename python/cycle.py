85134424ade1cd4297e0759e970c3c548b69ff36
fedor buyanov <fdbuyanov@gmail.com>
2021-12-01 17:05:02 -0500

    done

--- python/cycle.py
+++ python/cycle.py
@@ -1,8 +1,14 @@
-import random
+"""import random
 while True:
     print(random.randint(1,9))
     favnumber = input("Is this your favorite number?: ")
     if favnumber == "no":
         continue
     elif favnumber == "yes":
+        break"""
+counter = 0
+while True:
+    print(counter)
+    counter += 5
+    if counter == 105:
         break
