9dea380c034eec1bba45e75cfe6ca2cb17a2fbdf
fdbuyanov <fdbuyanov@gmail.com>
2021-12-08 17:05:22 -0500

    done

--- cycle1.py
+++ cycle1.py
@@ -1,8 +1,33 @@
+"""number = 20
 while True:
+    print(number)
+    number -= 1
+    if number < 0:
+        break"""
+
+"""conter = 2
+multiply = 1
+while True:
+    multiply *= conter 
+    conter += 2
+    if conter > 10:
+        break
+print(multiply)"""
+
+"""while True:
     number = int(input("Enter a number: "))
-    if number != 0:
-        print('This isnt zero!')
-        continue
-    print('Encountered zero')
-    break
-print('Program is continuing')
+    if number == 0:
+        break"""
+
+"""while True:
+    number = input("Enter a number: ")
+    if number == "stop":
+        break"""
+
+lines = 0
+while True:
+    thero = input("What's up: ")
+    lines += 1
+    if thero == "Thank you." or thero == "Thanks.":
+        break
+print(lines)
