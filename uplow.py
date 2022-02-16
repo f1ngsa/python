7a5e723d140aa2f47c2898d01f365966df99762e
fdbuyanov <fdbuyanov@gmail.com>
2022-02-16 17:06:11 -0500

    done

---
+++ uplow.py
@@ -0,0 +1,19 @@
+"""a = input("Enter a few words: ")
+if "up" in a.lower():
+    print(a.upper())
+elif "down" in a.lower():
+    print(a.lower())"""
+
+"""pas = input("Enter a password: ")
+if pas.isalnum() == True and pas.isalpha() == False and pas.isdigit() == False:
+    print("The password is good.")
+else:
+    print("This is a bad password.")"""
+
+"""a = input("Say something:")
+if a.lower().startswith("hello") or a.lower().startswith("hi"):
+    print("Hello!")
+elif a.lower().endswith("bye") or a.lower().endswith("goodbye"):
+    print("Goodbye!")"""
+
+a =
